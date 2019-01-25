bfd355226100b0cbce22480a0a2cb5742edd5452
MikeWang <it512@msn.com>
2019-01-26 00:19:06 +0800

    add DebugHandler

--- debug.go
+++ debug.go
@@ -51,3 +51,17 @@ func makeMethodReference(k string, md Metadata) *MethodReference {
 	}
 	return mr
 }
+
+type DebugHandler struct {
+	*MethodRepository
+}
+
+func Debug(mr *MethodRepository) *DebugHandler {
+	return &DebugHandler{
+		MethodRepository: mr,
+	}
+}
+
+func (d *DebugHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	d.ServeDebug(w, r)
+}
